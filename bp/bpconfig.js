4df8e88f7265edf44980f4334daea927ad175f91
Phil Archer <phila@w3.org>
2015-09-15 13:32:56 +0100

    Adding OGC logo, copyright etc.

--- bp/bpconfig.js
+++ bp/bpconfig.js
@@ -25,7 +25,14 @@ var respecConfig = {
       { 
         value: "William Roy"
       }]
-    }],
+    },
+    {
+      key: "OGC Document Number:",
+      data: [
+      {
+        value: "OGC 15-xxx"
+      }]
+      }],
     wg: "Spatial Data on the Web Working Group",
     wgURI: "http://www.w3.org/2015/spatial/",
     wgPublicList: "public-sdw-wg",
@@ -33,6 +40,25 @@ var respecConfig = {
     inlineCSS: true,
     noIDLIn: true,
     noLegacyStyle: false,
+      logos: [
+      {
+        src: "http://www.w3.org/Icons/w3c_home",
+        alt: "W3C",
+        height: "48",
+        width: "72",
+        url: "http://www.w3.org/"
+      },
+      {
+        src: "http://www.w3.org/2015/01/ogc_logo.jpg",
+        alt: "OGC",
+        height: "48",
+        width: "115",
+        url: "http://www.opengeospatial.org/"
+      }
+      ],
+    noRecTrack: true,
+    overrideCopyright: "<p class='copyright'><a href='http://www.w3.org/Consortium/Legal/ipr-notice#Copyright'>Copyright</a> © 2015 <a href='http://www.opengeospatial.org/'>OGC</a> &amp; <a href='http://www.w3.org/'> <abbr title='World Wide Web Consortium'>W3C</abbr> </a><sup>®</sup> (<a href='http://www.csail.mit.edu/'><abbr title='Massachusetts Institute of Technology'>MIT</abbr></a>, <a href='http://www.ercim.eu/'><abbr title='European Research Consortium for Informatics and Mathematics'>ERCIM</abbr></a>, <a href='http://www.keio.ac.jp/'>Keio</a>, <a href='http://ev.buaa.edu.cn/'>Beihang</a>), <abbr title='World Wide Web Consortium'>W3C</abbr> <a href='http://www.w3.org/Consortium/Legal/ipr-notice#Legal_Disclaimer'>liability</a>, <a href='http://www.w3.org/Consortium/Legal/ipr-notice#W3C_Trademarks'>trademark</a> and <a href='http://www.w3.org/Consortium/Legal/copyright-documents'>document use</a> rules apply.</p>",
+
 
     localBiblio: {
       "RDA" : {
