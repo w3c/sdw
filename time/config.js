59426c25b801ff9d37b216f7b5db1ccb5b9f5c79
Linda van den Brink <l.vandenbrink@geonovum.nl>
2020-03-12 14:36:02 +0100

    update OGC doc nr in config

--- time/config.js
+++ time/config.js
@@ -32,7 +32,7 @@ var respecConfig = {
       key: "OGC Document Number",
       data: [
       {
-        value: "OGC 16-071r2"
+        value: "OGC 16-071r3"
       }]
     }],
     wg: "Spatial Data on the Web Working Group",
