1c0fa5b8f2ec4ef158a40ac8b0da83af7d9dd13c
Malvoz <26493779+Malvoz@users.noreply.github.com>
2022-03-28 20:47:40 +0200

    Fix error in bpconfig.js
    Fix error: `Uncaught SyntaxError: Unexpected string`

--- bp/bpconfig.js
+++ bp/bpconfig.js
@@ -251,19 +251,19 @@ var respecConfig = {
         "title": "Web Data",
         "date": "1 November 2016"
       },
-      "FAIR-XG":{
-	"authors": ["Directorate-General for Research and Innovation"]      
-	"href":"https://doi.org/10.2777/1524",
-	"publisher": "Publications Office",
-	"title":"Turning FAIR into reality. Final report and action plan from the European Commission expert group on FAIR data",
-	"date": "2018"
+      "FAIR-XG": {
+        "authors": ["Directorate-General for Research and Innovation"],
+        "href":"https://doi.org/10.2777/1524",
+        "publisher": "Publications Office",
+        "title":"Turning FAIR into reality. Final report and action plan from the European Commission expert group on FAIR data",
+        "date": "2018"
       },
-      "FAIR-DMM":{
+      "FAIR-DMM": {
         "href":"https://doi.org/10.15497/rda00050",
         "publisher": "Research Data Alliance",
-	"title":"FAIR Data Maturity Model. Specification and Guidelines (1.0)",
+        "title":"FAIR Data Maturity Model. Specification and Guidelines (1.0)",
         "date": "2020"
-      },	    
+      },
 
 // ALREADY IN SPECREF
 
